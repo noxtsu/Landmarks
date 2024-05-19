1faf85754f0c02e4283cd66a57ba067b6107903b
ryota.nozu <kuraraiban29@gmail.com>
2024-05-19 23:50:27 +0900

    change text "Turtle Rock"

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, Swift!!!")
+            Text("Turtle Rock")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
         }
         .padding()
     }
