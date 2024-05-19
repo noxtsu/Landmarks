b95fc9b580dab1699aec911e9f36b3781aa9c349
ryota.nozu <kuraraiban29@gmail.com>
2024-05-19 23:28:42 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ryota.nozu on 2024/05/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
