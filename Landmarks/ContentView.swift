09d6f97bedbf84263a74d542602d90ff2e29adab
ryota.nozu <kuraraiban29@gmail.com>
2024-05-20 00:05:58 +0900

    add text lokation

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
